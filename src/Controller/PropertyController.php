7627ceb322f40b8c2469b3eb186a6d60d88d9cda
Ludovic Braine <ludovic.braine@winciel.fr>
2020-04-21 18:05:18 +0200

    Pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -6,7 +6,9 @@ use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,10 +34,17 @@ class PropertyController extends AbstractController
      * @Route("/biens", name="property.index")
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        //Crée une entité qui va représenter notre recherche
+        //Crée un formulaire
+        //Gérer le traitement dans le controller
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery(),
+            $request->query->getInt('page',1), 12);
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $properties
         ]);
     }
 
